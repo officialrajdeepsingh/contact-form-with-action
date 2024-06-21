0517edd77e2662ae9a5701bc91a2605a2ebe00e5
Rajdeep Singh <radewala.2@gmail.com>
2024-06-21 17:20:49 +0530

    Adding Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Toaster richColors closeButton position="top-right" />
         {children}
       </body>
     </html>
