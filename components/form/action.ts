7f5ed89d83ff979a02b0ef6c90f1d72020b2b7e3
Rajdeep Singh <radewala.2@gmail.com>
2024-07-01 17:31:18 +0530

    remove the console log

--- components/form/action.ts
+++ components/form/action.ts
@@ -5,13 +5,6 @@ import { z } from "zod";
 import { FormDataReturn } from "@/types";
 
 export default async function contactAction(_prevState: any, params: FormData) {
-  console.log("Action start :");
-  for (let i = 0; i < 1000000; i++) {
-    let temp = i * i;
-  }
-
-  console.log(" form schema :");
-
   const validation = await formSchema.safeParseAsync({
     firstName: params.get("first-name"),
     lastName: params.get("last-name"),
